fa2481d114eefc5fed5f685381c81ae6608ac6c2
Fluxo <fluxo.soft@gmail.com>
2024-08-17 08:13:31 +0200

    Update settings.js

--- src/application/settings.js
+++ src/application/settings.js
@@ -1,9 +1,9 @@
 export const getOauthEndPoint = () => {
-  return window.location.href.indexOf('localhost') === -1 ? 'https://fluxo-api.herokuapp.com/oauth/login' : 'http://localhost:3100/oauth/login'
+  return window.location.href.indexOf('localhost') === -1 ? 'https://fluxo-api.onrender.com/oauth/login' : 'http://localhost:3100/oauth/login'
 }
 
 export const getApiEndPoint = (isDemo) => {
-  const apiEndPoint = window.location.href.indexOf('localhost') === -1 ? 'https://fluxo-api.herokuapp.com' : 'http://localhost:3100'
+  const apiEndPoint = window.location.href.indexOf('localhost') === -1 ? 'https://fluxo-api.onrender.com' : 'http://localhost:3100'
   return isDemo ? `${apiEndPoint}/fake` : `${apiEndPoint}/api`
 }
 
